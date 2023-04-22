ccfff5006a6baf7eca4b9f7bc1c9f73483d1fa54
Tom Lawton <tom.lawton@cndu.it>
2023-04-22 18:54:50 +0100

    chore: do not set script_dir for remote installer

--- remote-install.sh
+++ remote-install.sh
@@ -3,8 +3,6 @@
 set -Eeuo pipefail
 trap cleanup SIGINT SIGTERM ERR EXIT
 
-script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
-
 setup_colors() {
   if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
     NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
