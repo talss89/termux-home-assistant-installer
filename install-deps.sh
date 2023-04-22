26e3a940e82bd03cf5a73c31a306e9d3ac691de7
Tom Lawton <tom.lawton@cndu.it>
2023-04-22 13:44:06 +0100

    Fix ffmpeg installation

--- install-deps.sh
+++ install-deps.sh
@@ -4,7 +4,8 @@ pkg upgrade -y
 
 pkg i tsu python nano termux-api make libjpeg-turbo make git rust python-cryptography libcrypt libffi binutils mosquitto wget libsodium python-numpy -y
 
-dpkg i ./contrib/ffmpeg_5.1.2-7_aarch64.deb
+dpkg -i ./contrib/ffmpeg_5.1.2-7_aarch64.deb
+apt install -f
 
 python -m venv --without-pip hass
 source hass/bin/activate
@@ -15,8 +16,8 @@ pip install maturin
 pip install setuptools
 MATHLIB=m pip install aiohttp_cors==0.7.0
 MATHLIB=m pip install PyTurboJPEG==1.6.7
-
 pip install git+https://github.com/amitdev/lru-dict@5013406c409a0a143a315146df388281bfb2172d
+
 SODIUM_INSTALL=system pip install pynacl
 
 RUSTFLAGS="-C lto=n" CARGO_BUILD_TARGET="$(rustc -Vv | grep "host" | awk '{print $2}')"  CRYPTOGRAPHY_DONT_BUILD_RUST=1 pip install homeassistant==2023.4.4
