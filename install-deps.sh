640c518545d4e00b76150e02dd86b84648d8adb2
Tom Lawton <tom.lawton@cndu.it>
2023-04-22 17:54:53 +0100

    APT / DPKG force keep original

--- install-deps.sh
+++ install-deps.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
+
+DPKG_INSTALL_FLAGS="--force-confold"
+APT_INSTALL_FLAGS="-o Dpkg::Options::=\"$DPKG_INSTALL_FLAGS\" --assume-yes"
+
 pkg update
-pkg upgrade -y
+pkg upgrade $APT_INSTALL_FLAGS
 
-pkg i tsu python nano termux-api make libjpeg-turbo make git rust python-cryptography libcrypt libffi binutils mosquitto wget libsodium python-numpy -y
+pkg i tsu python nano termux-api make libjpeg-turbo make git rust python-cryptography libcrypt libffi binutils mosquitto wget libsodium python-numpy $APT_INSTALL_FLAGS
 
-dpkg -i ./contrib/ffmpeg_5.1.2-7_aarch64.deb
-apt install -f -y
+dpkg -i $DPKG_INSTALL_FLAGS ./contrib/ffmpeg_5.1.2-7_aarch64.deb
+apt install -f $APT_INSTALL_FLAGS
 
 python -m venv --without-pip hass
 source hass/bin/activate
@@ -22,3 +26,6 @@ pip install git+https://github.com/amitdev/lru-dict@5013406c409a0a143a315146df38
 SODIUM_INSTALL=system pip install pynacl
 
 RUSTFLAGS="-C lto=n" CARGO_BUILD_TARGET="$(rustc -Vv | grep "host" | awk '{print $2}')"  CRYPTOGRAPHY_DONT_BUILD_RUST=1 pip install homeassistant==2023.4.4
+
+# Run `hass` in check config mode, purely to install dependencies, and then quit.
+hass --script check_config
